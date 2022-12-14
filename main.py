d5d3ffd7dbf1dcd3f58bdccda28a47e8c1b77caf
hind333 <109863744+hind333@users.noreply.github.com>
2022-12-14 20:06:42 +0300

    تم

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 
 # write your code here
+class Person:
+    # name = 'Boo'
+    # age = 2
+    def __init__(self, name ,age) :
+        self.name = name
+        self.age = age
+    def is_adult(self):
+        if self.age >= 18:
+            print('You have too much responsibilities')
+        if self.age <= 18:
+            print('Lucky you')   
+    def __str__(self):
+        return f'My name is {self.name} and I am {self.age} years old '               
+first_person = Person('boo',2)
+print(first_person.age,first_person.name)
+first_person.is_adult()
+print(first_person)
